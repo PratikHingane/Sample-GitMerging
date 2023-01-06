a3451af5136a3445810c3bb8bb06494c0f1f1bd9
SUNILRAO MULEY <NDH00886@Nmcorp.Nissan.Biz>
2023-01-06 16:07:35 +0530

    prajot folders aded

---
+++ SampleGit/src/app/prajot/prajot.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-prajot',
+  templateUrl: './prajot.component.html',
+  styleUrls: ['./prajot.component.scss']
+})
+export class PrajotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
