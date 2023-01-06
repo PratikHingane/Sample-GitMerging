020c1d1a68be22d3d98cab1b56ed4cfd3546a729
HINGANE <NDH00877@Nmcorp.Nissan.Biz>
2023-01-06 15:52:22 +0530

    Pratik component added

--- SampleGit/src/app/app.module.ts
+++ SampleGit/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PratikComponent } from './pratik/pratik.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PratikComponent
   ],
   imports: [
     BrowserModule,
---
+++ SampleGit/src/app/pratik/pratik.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pratik',
+  templateUrl: './pratik.component.html',
+  styleUrls: ['./pratik.component.scss']
+})
+export class PratikComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
